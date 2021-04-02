e3190742cab6f3976637e94f0eb7cfe5489c1ef6
Lucas Panassolo <lucaspanassolo12@hotmail.com>
2021-04-02 20:15:47 -0300

    Slider changes automatically

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -17,41 +17,72 @@ export const Background: React.FC = () => {
     const [selectedExplainOption, setSelectedExplainOption] = useState<ExplainOption>();
     const [explainOptions, setExplainOptions] = useState<ExplainOption[]>()
 
+    const nextOption = useCallback(() => {
+        function getOption() {
+            if (!explainOptions) return null;
+
+            if (explainOptions.sort((a, b) => b.id - a.id)[0].id === selectedExplainOption?.id) {
+                return explainOptions.find(option => option.id === 1);
+            }
+
+            return explainOptions.find((value) => value.id === ((selectedExplainOption?.id ?? 0) + 1));
+        }
+
+        if (explainOptions) {
+            setSelectedExplainOption(getOption() ?? {} as ExplainOption);
+        }
+
+    }, [explainOptions, selectedExplainOption])
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            nextOption();
+        }, 8000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [nextOption]);
+
     useEffect(() => {
         // Static values
         const options = [
             {
                 id: 1,
-                title: 'Marcenas mattis egestas',
+                title: 'Marcenas mattis egestas 1',
                 description: 'Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.',
                 image: leftBackgroundImage
             },
             {
                 id: 2,
-                title: 'Marcenas mattis egestas',
+                title: 'Marcenas mattis egestas 2',
                 description: 'Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.',
                 image: leftBackgroundImage
             },
             {
                 id: 3,
-                title: 'Marcenas mattis egestas',
+                title: 'Marcenas mattis egestas 3',
                 description: 'Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.',
                 image: leftBackgroundImage
             },
             {
                 id: 4,
-                title: 'Marcenas mattis egestas',
+                title: 'Marcenas mattis egestas 4',
                 description: 'Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.',
                 image: leftBackgroundImage
             },
         ]
         setExplainOptions(options);
         setSelectedExplainOption(options[0]);
-    }, [])
+    }, []);
 
     const handleChangeSelectedOption = useCallback((id: number) => {
         if (explainOptions)
             setSelectedExplainOption(explainOptions.find(option => option.id === id));
+    }, [explainOptions])
+
+    const sortedOptions = useCallback(() => {
+        return explainOptions?.sort((a, b) => a.id - b.id);
     }, [explainOptions])
 
     return (
@@ -62,7 +93,7 @@ export const Background: React.FC = () => {
                 <ExplainDescription>{selectedExplainOption?.description}</ExplainDescription>
 
                 <TabSelection>
-                    {explainOptions && explainOptions.map((option) => (
+                    {explainOptions && sortedOptions()?.map((option) => (
                         <TabOption key={option.id} isSelected={option.id === selectedExplainOption?.id} onClick={handleChangeSelectedOption} id={option.id} />
                     ))}
                 </TabSelection>
